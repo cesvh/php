300a4b231b49c5255385f17bc5175a9075297f5c
cesvh <cesar.vega.hdez@gmail.com>
2024-07-14 14:37:33 -0600

    admin-template install

---
+++ laravel/projects/web/tutos/___logs/admin-template.sh
@@ -0,0 +1,323 @@
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\admin-template>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\admin-template>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\admin-template>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\admin-template> composer --version
+Composer version 2.7.2 2024-03-11 17:12:18
+PHP version 8.2.13 (C:\wamp64\bin\php\php8.2.13\php.exe)
+Run the "diagnose" command to get more detailed diagnostics output.
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\admin-template>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\admin-template>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\admin-template>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\admin-template> composer create-project laravel/laravel admin-template
+Creating a "laravel/laravel" project at "./admin-template"
+Installing laravel/laravel (v11.1.3)
+  - Downloading laravel/laravel (v11.1.3)
+  - Installing laravel/laravel (v11.1.3): Extracting archive
+Created project in C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\admin-template\admin-template
+> @php -r "file_exists('.env') || copy('.env.example', '.env');"
+Loading composer repositories with package information
+Updating dependencies
+Lock file operations: 107 installs, 0 updates, 0 removals
+  - Locking brick/math (0.12.1)
+  - Locking carbonphp/carbon-doctrine-types (3.2.0)
+  - Locking dflydev/dot-access-data (v3.0.3)
+  - Locking doctrine/inflector (2.0.10)
+  - Locking doctrine/lexer (3.0.1)
+  - Locking dragonmantank/cron-expression (v3.3.3)
+  - Locking egulias/email-validator (4.0.2)
+  - Locking fakerphp/faker (v1.23.1)
+  - Locking filp/whoops (2.15.4)
+  - Locking fruitcake/php-cors (v1.3.0)
+  - Locking graham-campbell/result-type (v1.1.2)
+  - Locking guzzlehttp/guzzle (7.8.1)
+  - Locking guzzlehttp/promises (2.0.2)
+  - Locking guzzlehttp/psr7 (2.6.2)
+  - Locking guzzlehttp/uri-template (v1.0.3)
+  - Locking hamcrest/hamcrest-php (v2.0.1)
+  - Locking laravel/framework (v11.15.0)
+  - Locking laravel/pint (v1.16.2)
+  - Locking laravel/prompts (v0.1.24)
+  - Locking laravel/sail (v1.30.2)
+  - Locking laravel/serializable-closure (v1.3.3)
+  - Locking laravel/tinker (v2.9.0)
+  - Locking league/commonmark (2.4.2)
+  - Locking league/config (v1.2.0)
+  - Locking league/flysystem (3.28.0)
+  - Locking league/flysystem-local (3.28.0)
+  - Locking league/mime-type-detection (1.15.0)
+  - Locking mockery/mockery (1.6.12)
+  - Locking monolog/monolog (3.7.0)
+  - Locking myclabs/deep-copy (1.12.0)
+  - Locking nesbot/carbon (3.6.0)
+  - Locking nette/schema (v1.3.0)
+  - Locking nette/utils (v4.0.4)
+  - Locking nikic/php-parser (v5.1.0)
+  - Locking nunomaduro/collision (v8.1.1)
+  - Locking nunomaduro/termwind (v2.0.1)
+  - Locking phar-io/manifest (2.0.4)
+  - Locking phar-io/version (3.2.1)
+  - Locking phpoption/phpoption (1.9.2)
+  - Locking phpunit/php-code-coverage (11.0.5)
+  - Locking phpunit/php-file-iterator (5.0.1)
+  - Locking phpunit/php-invoker (5.0.1)
+  - Locking phpunit/php-text-template (4.0.1)
+  - Locking phpunit/php-timer (7.0.1)
+  - Locking phpunit/phpunit (11.2.7)
+  - Locking psr/clock (1.0.0)
+  - Locking psr/container (2.0.2)
+  - Locking psr/event-dispatcher (1.0.0)
+  - Locking psr/http-client (1.0.3)
+  - Locking psr/http-factory (1.1.0)
+  - Locking psr/http-message (2.0)
+  - Locking psr/log (3.0.0)
+  - Locking psr/simple-cache (3.0.0)
+  - Locking psy/psysh (v0.12.4)
+  - Locking ralouphie/getallheaders (3.0.3)
+  - Locking ramsey/collection (2.0.0)
+  - Locking ramsey/uuid (4.7.6)
+  - Locking sebastian/cli-parser (3.0.2)
+  - Locking sebastian/code-unit (3.0.1)
+  - Locking sebastian/code-unit-reverse-lookup (4.0.1)
+  - Locking sebastian/comparator (6.0.1)
+  - Locking sebastian/complexity (4.0.1)
+  - Locking sebastian/diff (6.0.2)
+  - Locking sebastian/environment (7.2.0)
+  - Locking sebastian/exporter (6.1.3)
+  - Locking sebastian/global-state (7.0.2)
+  - Locking sebastian/lines-of-code (3.0.1)
+  - Locking sebastian/object-enumerator (6.0.1)
+  - Locking sebastian/object-reflector (4.0.1)
+  - Locking sebastian/recursion-context (6.0.2)
+  - Locking sebastian/type (5.0.1)
+  - Locking sebastian/version (5.0.1)
+  - Locking symfony/clock (v7.1.1)
+  - Locking symfony/console (v7.1.2)
+  - Locking symfony/css-selector (v7.1.1)
+  - Locking symfony/deprecation-contracts (v3.5.0)
+  - Locking symfony/error-handler (v7.1.2)
+  - Locking symfony/event-dispatcher (v7.1.1)
+  - Locking symfony/event-dispatcher-contracts (v3.5.0)
+  - Locking symfony/finder (v7.1.1)
+  - Locking symfony/http-foundation (v7.1.1)
+  - Locking symfony/http-kernel (v7.1.2)
+  - Locking symfony/mailer (v7.1.2)
+  - Locking symfony/mime (v7.1.2)
+  - Locking symfony/polyfill-ctype (v1.30.0)
+  - Locking symfony/polyfill-intl-grapheme (v1.30.0)
+  - Locking symfony/polyfill-intl-idn (v1.30.0)
+  - Locking symfony/polyfill-intl-normalizer (v1.30.0)
+  - Locking symfony/polyfill-mbstring (v1.30.0)
+  - Locking symfony/polyfill-php72 (v1.30.0)
+  - Locking symfony/polyfill-php80 (v1.30.0)
+  - Locking symfony/polyfill-php83 (v1.30.0)
+  - Locking symfony/polyfill-uuid (v1.30.0)
+  - Locking symfony/process (v7.1.1)
+  - Locking symfony/routing (v7.1.1)
+  - Locking symfony/service-contracts (v3.5.0)
+  - Locking symfony/string (v7.1.2)
+  - Locking symfony/translation (v7.1.1)
+  - Locking symfony/translation-contracts (v3.5.0)
+  - Locking symfony/uid (v7.1.1)
+  - Locking symfony/var-dumper (v7.1.2)
+  - Locking symfony/yaml (v7.1.1)
+  - Locking theseer/tokenizer (1.2.3)
+  - Locking tijsverkoyen/css-to-inline-styles (v2.2.7)
+  - Locking vlucas/phpdotenv (v5.6.0)
+  - Locking voku/portable-ascii (2.0.1)
+  - Locking webmozart/assert (1.11.0)
+Writing lock file
+Installing dependencies from lock file (including require-dev)
+Package operations: 107 installs, 0 updates, 0 removals
+  - Downloading symfony/polyfill-ctype (v1.30.0)
+  - Downloading symfony/polyfill-php83 (v1.30.0)
+  - Downloading symfony/polyfill-mbstring (v1.30.0)
+  - Downloading symfony/polyfill-php80 (v1.30.0)
+  - Downloading laravel/pint (v1.16.2)
+  - Downloading symfony/polyfill-intl-normalizer (v1.30.0)
+  - Downloading symfony/polyfill-intl-grapheme (v1.30.0)
+  - Downloading symfony/string (v7.1.2)
+  - Downloading symfony/console (v7.1.2)
+  - Downloading symfony/var-dumper (v7.1.2)
+  - Downloading symfony/polyfill-uuid (v1.30.0)
+  - Downloading symfony/polyfill-php72 (v1.30.0)
+  - Downloading symfony/polyfill-intl-idn (v1.30.0)
+  - Downloading symfony/mime (v7.1.2)
+  - Downloading symfony/mailer (v7.1.2)
+  - Downloading symfony/error-handler (v7.1.2)
+  - Downloading symfony/http-kernel (v7.1.2)
+  - Downloading nesbot/carbon (3.6.0)
+  - Downloading monolog/monolog (3.7.0)
+  - Downloading dflydev/dot-access-data (v3.0.3)
+  - Downloading laravel/prompts (v0.1.24)
+  - Downloading laravel/framework (v11.15.0)
+  - Downloading laravel/sail (v1.30.2)
+  - Downloading nikic/php-parser (v5.1.0)
+  - Downloading sebastian/version (5.0.1)
+  - Downloading sebastian/type (5.0.1)
+  - Downloading sebastian/recursion-context (6.0.2)
+  - Downloading sebastian/object-reflector (4.0.1)
+  - Downloading sebastian/object-enumerator (6.0.1)
+  - Downloading sebastian/global-state (7.0.2)
+  - Downloading sebastian/exporter (6.1.3)
+  - Downloading sebastian/environment (7.2.0)
+  - Downloading sebastian/diff (6.0.2)
+  - Downloading sebastian/comparator (6.0.1)
+  - Downloading sebastian/code-unit (3.0.1)
+  - Downloading sebastian/cli-parser (3.0.2)
+  - Downloading phpunit/php-timer (7.0.1)
+  - Downloading phpunit/php-text-template (4.0.1)
+  - Downloading phpunit/php-invoker (5.0.1)
+  - Downloading phpunit/php-file-iterator (5.0.1)
+  - Downloading sebastian/lines-of-code (3.0.1)
+  - Downloading sebastian/complexity (4.0.1)
+  - Downloading sebastian/code-unit-reverse-lookup (4.0.1)
+  - Downloading phpunit/php-code-coverage (11.0.5)
+  - Downloading myclabs/deep-copy (1.12.0)
+  - Downloading phpunit/phpunit (11.2.7)
+  - Installing doctrine/inflector (2.0.10): Extracting archive
+  - Installing doctrine/lexer (3.0.1): Extracting archive
+  - Installing symfony/polyfill-ctype (v1.30.0): Extracting archive
+  - Installing webmozart/assert (1.11.0): Extracting archive
+  - Installing dragonmantank/cron-expression (v3.3.3): Extracting archive
+  - Installing symfony/deprecation-contracts (v3.5.0): Extracting archive
+  - Installing psr/container (2.0.2): Extracting archive
+  - Installing fakerphp/faker (v1.23.1): Extracting archive
+  - Installing symfony/polyfill-php83 (v1.30.0): Extracting archive
+  - Installing symfony/polyfill-mbstring (v1.30.0): Extracting archive
+  - Installing symfony/http-foundation (v7.1.1): Extracting archive
+  - Installing fruitcake/php-cors (v1.3.0): Extracting archive
+  - Installing psr/http-message (2.0): Extracting archive
+  - Installing psr/http-client (1.0.3): Extracting archive
+  - Installing ralouphie/getallheaders (3.0.3): Extracting archive
+  - Installing psr/http-factory (1.1.0): Extracting archive
+  - Installing guzzlehttp/psr7 (2.6.2): Extracting archive
+  - Installing guzzlehttp/promises (2.0.2): Extracting archive
+  - Installing guzzlehttp/guzzle (7.8.1): Extracting archive
+  - Installing symfony/polyfill-php80 (v1.30.0): Extracting archive
+  - Installing guzzlehttp/uri-template (v1.0.3): Extracting archive
+  - Installing laravel/pint (v1.16.2): Extracting archive
+  - Installing symfony/polyfill-intl-normalizer (v1.30.0): Extracting archive
+  - Installing symfony/polyfill-intl-grapheme (v1.30.0): Extracting archive
+  - Installing symfony/string (v7.1.2): Extracting archive
+  - Installing symfony/service-contracts (v3.5.0): Extracting archive
+  - Installing symfony/console (v7.1.2): Extracting archive
+  - Installing voku/portable-ascii (2.0.1): Extracting archive
+  - Installing phpoption/phpoption (1.9.2): Extracting archive
+  - Installing graham-campbell/result-type (v1.1.2): Extracting archive
+  - Installing vlucas/phpdotenv (v5.6.0): Extracting archive
+  - Installing symfony/css-selector (v7.1.1): Extracting archive
+  - Installing tijsverkoyen/css-to-inline-styles (v2.2.7): Extracting archive
+  - Installing symfony/var-dumper (v7.1.2): Extracting archive
+  - Installing symfony/polyfill-uuid (v1.30.0): Extracting archive
+  - Installing symfony/uid (v7.1.1): Extracting archive
+  - Installing symfony/routing (v7.1.1): Extracting archive
+  - Installing symfony/process (v7.1.1): Extracting archive
+  - Installing symfony/polyfill-php72 (v1.30.0): Extracting archive
+  - Installing symfony/polyfill-intl-idn (v1.30.0): Extracting archive
+  - Installing symfony/mime (v7.1.2): Extracting archive
+  - Installing psr/event-dispatcher (1.0.0): Extracting archive
+  - Installing symfony/event-dispatcher-contracts (v3.5.0): Extracting archive
+  - Installing symfony/event-dispatcher (v7.1.1): Extracting archive
+  - Installing psr/log (3.0.0): Extracting archive
+  - Installing egulias/email-validator (4.0.2): Extracting archive
+  - Installing symfony/mailer (v7.1.2): Extracting archive
+  - Installing symfony/error-handler (v7.1.2): Extracting archive
+  - Installing symfony/http-kernel (v7.1.2): Extracting archive
+  - Installing symfony/finder (v7.1.1): Extracting archive
+  - Installing ramsey/collection (2.0.0): Extracting archive
+  - Installing brick/math (0.12.1): Extracting archive
+  - Installing ramsey/uuid (4.7.6): Extracting archive
+  - Installing psr/simple-cache (3.0.0): Extracting archive
+  - Installing nunomaduro/termwind (v2.0.1): Extracting archive
+  - Installing symfony/translation-contracts (v3.5.0): Extracting archive
+  - Installing symfony/translation (v7.1.1): Extracting archive
+  - Installing psr/clock (1.0.0): Extracting archive
+  - Installing symfony/clock (v7.1.1): Extracting archive
+  - Installing carbonphp/carbon-doctrine-types (3.2.0): Extracting archive
+  - Installing nesbot/carbon (3.6.0): Extracting archive
+  - Installing monolog/monolog (3.7.0): Extracting archive
+  - Installing league/mime-type-detection (1.15.0): Extracting archive
+  - Installing league/flysystem (3.28.0): Extracting archive
+  - Installing league/flysystem-local (3.28.0): Extracting archive
+  - Installing nette/utils (v4.0.4): Extracting archive
+  - Installing nette/schema (v1.3.0): Extracting archive
+  - Installing dflydev/dot-access-data (v3.0.3): Extracting archive
+  - Installing league/config (v1.2.0): Extracting archive
+  - Installing league/commonmark (2.4.2): Extracting archive
+  - Installing laravel/serializable-closure (v1.3.3): Extracting archive
+  - Installing laravel/prompts (v0.1.24): Extracting archive
+  - Installing laravel/framework (v11.15.0): Extracting archive
+  - Installing symfony/yaml (v7.1.1): Extracting archive
+  - Installing laravel/sail (v1.30.2): Extracting archive
+  - Installing nikic/php-parser (v5.1.0): Extracting archive
+  - Installing psy/psysh (v0.12.4): Extracting archive
+  - Installing laravel/tinker (v2.9.0): Extracting archive
+  - Installing hamcrest/hamcrest-php (v2.0.1): Extracting archive
+  - Installing mockery/mockery (1.6.12): Extracting archive
+  - Installing filp/whoops (2.15.4): Extracting archive
+  - Installing nunomaduro/collision (v8.1.1): Extracting archive
+  - Installing sebastian/version (5.0.1): Extracting archive
+  - Installing sebastian/type (5.0.1): Extracting archive
+  - Installing sebastian/recursion-context (6.0.2): Extracting archive
+  - Installing sebastian/object-reflector (4.0.1): Extracting archive
+  - Installing sebastian/object-enumerator (6.0.1): Extracting archive
+  - Installing sebastian/global-state (7.0.2): Extracting archive
+  - Installing sebastian/exporter (6.1.3): Extracting archive
+  - Installing sebastian/environment (7.2.0): Extracting archive
+  - Installing sebastian/diff (6.0.2): Extracting archive
+  - Installing sebastian/comparator (6.0.1): Extracting archive
+  - Installing sebastian/code-unit (3.0.1): Extracting archive
+  - Installing sebastian/cli-parser (3.0.2): Extracting archive
+  - Installing phpunit/php-timer (7.0.1): Extracting archive
+  - Installing phpunit/php-text-template (4.0.1): Extracting archive
+  - Installing phpunit/php-invoker (5.0.1): Extracting archive
+  - Installing phpunit/php-file-iterator (5.0.1): Extracting archive
+  - Installing theseer/tokenizer (1.2.3): Extracting archive
+  - Installing sebastian/lines-of-code (3.0.1): Extracting archive
+  - Installing sebastian/complexity (4.0.1): Extracting archive
+  - Installing sebastian/code-unit-reverse-lookup (4.0.1): Extracting archive
+  - Installing phpunit/php-code-coverage (11.0.5): Extracting archive
+  - Installing phar-io/version (3.2.1): Extracting archive
+  - Installing phar-io/manifest (2.0.4): Extracting archive
+  - Installing myclabs/deep-copy (1.12.0): Extracting archive
+  - Installing phpunit/phpunit (11.2.7): Extracting archive
+47 package suggestions were added by new dependencies, use `composer suggest` to see details.
+Generating optimized autoload files
+> Illuminate\Foundation\ComposerScripts::postAutoloadDump
+> @php artisan package:discover --ansi
+
+   INFO  Discovering packages.
+
+  laravel/sail ................................................................................................................................ DONE
+  laravel/tinker .............................................................................................................................. DONE
+  nesbot/carbon ............................................................................................................................... DONE
+  nunomaduro/collision ........................................................................................................................ DONE
+  nunomaduro/termwind ......................................................................................................................... DONE
+
+79 packages you are using are looking for funding.
+Use the `composer fund` command to find out more!
+> @php artisan vendor:publish --tag=laravel-assets --ansi --force
+
+   INFO  No publishable resources for tag [laravel-assets].
+
+No security vulnerability advisories found.
+> @php artisan key:generate --ansi
+
+   INFO  Application key set successfully.
+
+> @php -r "file_exists('database/database.sqlite') || touch('database/database.sqlite');"
+> @php artisan migrate --graceful --ansi
+
+   INFO  Preparing database.
+
+  Creating migration table ............................................................................................................ 24.72ms DONE
+
+   INFO  Running migrations.
+
+  0001_01_01_000000_create_users_table ................................................................................................ 59.92ms DONE
+  0001_01_01_000001_create_cache_table ................................................................................................ 14.27ms DONE
+  0001_01_01_000002_create_jobs_table ................................................................................................. 33.01ms DONE
+
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\admin-template>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\admin-template>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\admin-template>
