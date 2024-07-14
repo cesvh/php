26612248dbd180d58b248213e9bf07003026cdf1
cesvh <cesar.vega.hdez@gmail.com>
2024-07-14 16:08:14 -0600

    admin-template finished

--- laravel/projects/web/tutos/admin-template/admin-template/routes/web.php
+++ laravel/projects/web/tutos/admin-template/admin-template/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('dashboard', [UserController::class, 'dashboard']);
+Route::get('users', [UserController::class, 'users']);
