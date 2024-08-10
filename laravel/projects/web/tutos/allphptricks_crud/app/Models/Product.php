85ae4ea34440c67b98a98d40f1921b6dfac0bc11
cesvh <cesar.vega.hdez@gmail.com>
2024-08-10 15:20:47 -0600

    routes and model

--- laravel/projects/web/tutos/allphptricks_crud/app/Models/Product.php
+++ laravel/projects/web/tutos/allphptricks_crud/app/Models/Product.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'code',
+        'name',
+        'quantity',
+        'price',
+        'description'
+    ];
+    // En el código anterior, pasamos los nombres de las columnas a la matriz protegida $fillable para permitir la asignación masiva de estos campos; de lo contrario, Laravel no agregará registros a la tabla de productos si usamos Laravel Eloquent para agregar datos.
 }
