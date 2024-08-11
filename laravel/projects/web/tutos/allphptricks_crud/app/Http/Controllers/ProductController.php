dfbb64866534b774b86abd42f45c4a0fb3edba85
cesvh <cesar.vega.hdez@gmail.com>
2024-08-10 18:28:27 -0600

    Controller, vistas, request, validations

--- laravel/projects/web/tutos/allphptricks_crud/app/Http/Controllers/ProductController.php
+++ laravel/projects/web/tutos/allphptricks_crud/app/Http/Controllers/ProductController.php
@@ -5,62 +5,70 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use App\Http\Requests\StoreProductRequest;
 use App\Http\Requests\UpdateProductRequest;
+use Illuminate\Http\View;
+use Illuminate\Http\RedirectResponse;
 
 class ProductController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Mostrar todos nuestros productos de la tabla de productos.
      */
     public function index()
     {
-        //
+        return view('products.index', [
+            'products' => Product::latest()->paginate(2)
+        ]);
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Para mostrar agregar nuevo formulario de producto.
      */
     public function create()
     {
-        //
+        return view('products.create');
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Para almacenar nuestros nuevos datos de solicitud de formulario de producto en nuestra tabla de productos.
      */
-    public function store(StoreProductRequest $request)
+    public function store(StoreProductRequest $request) : RedirectResponse
     {
-        //
+        Product::create($request->validated());
+        return redirect()->route('products.index')->withSuccess('¡Producto agregado con éxito!');
     }
 
     /**
-     * Display the specified resource.
+     * Para mostrar un solo producto con detalles.
      */
     public function show(Product $product)
     {
-        //
+        return view('products.show', compact('product'));
     }
 
     /**
-     * Show the form for editing the specified resource.
+     * Para mostrar un formulario de edición de producto.
      */
     public function edit(Product $product)
     {
-        //
+        return view('products.edit', compact('product'));
     }
 
     /**
-     * Update the specified resource in storage.
+     * Para actualizar los datos del producto en nuestra tabla de productos.
      */
-    public function update(UpdateProductRequest $request, Product $product)
+    public function update(UpdateProductRequest $request, Product $product) : RedirectResponse
     {
-        //
+        $product->update($request->validated());
+        return redirect()->back()->withSuccess('Producto actualizado con éxito.');
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Para eliminar el producto de la base de datos.
      */
-    public function destroy(Product $product)
+    public function destroy(Product $product) : RedirectResponse
     {
-        //
+        $product->delete();
+        return redirect()->route('products.index')
+                ->withSuccess('Producto eliminado con éxito.');
     }
 }
