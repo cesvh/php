3c1a64fb446b2f106a88614e02737fd0521a0203
cesvh <cesar.vega.hdez@gmail.com>
2024-08-31 00:46:24 -0600

    controller, model, routes

--- laravel/projects/web/tutos/itsolutionstuff-crud/routes/web.php
+++ laravel/projects/web/tutos/itsolutionstuff-crud/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Controller\ProductController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::resource('products', ProductController::class);
