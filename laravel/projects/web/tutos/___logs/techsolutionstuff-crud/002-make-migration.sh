e8fd85f4189af41e372e10b45569a4f726223d7a
cesvh <cesar.vega.hdez@gmail.com>
2024-09-02 22:51:26 -0600

    make migration

---
+++ laravel/projects/web/tutos/___logs/techsolutionstuff-crud/002-make-migration.sh
@@ -0,0 +1,33 @@
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud> php artisan make:migration create_items_table --create=items
+
+   INFO  Migration [C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud\database\migrations/2024_09_03_043559_create_items_table.php] created successfully.
+
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud> php artisan migrate
+
+   INFO  Preparing database.
+
+  Creating migration table ............................................................................................................ 20.15ms DONE
+
+   INFO  Running migrations.
+
+  0001_01_01_000000_create_users_table ............................................................................................... 231.05ms DONE
+  0001_01_01_000001_create_cache_table ................................................................................................ 25.75ms DONE
+  0001_01_01_000002_create_jobs_table ................................................................................................ 101.65ms DONE
+  2024_09_03_043559_create_items_table ................................................................................................ 15.72ms DONE
+
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
