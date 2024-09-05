851ba7b429f62268dc31ca51c143e205da4c806e
cesvh <cesar.vega.hdez@gmail.com>
2024-09-04 22:57:17 -0600

    Request Validation logs

---
+++ laravel/projects/web/tutos/___logs/techsolutionstuff-crud/003-request-validation.sh
@@ -0,0 +1,68 @@
+Windows PowerShell
+Copyright (C) Microsoft Corporation. Todos los derechos reservados.
+
+Instale la versión más reciente de PowerShell para obtener nuevas características y mejoras. https://aka.ms/PSWindows
+
+PS C:\Users\cesvh>
+PS C:\Users\cesvh>
+PS C:\Users\cesvh>
+PS C:\Users\cesvh>
+PS C:\Users\cesvh>
+PS C:\Users\cesvh> cd C:\Users\cesvh\dev\github\php
+PS C:\Users\cesvh\dev\github\php>
+PS C:\Users\cesvh\dev\github\php>
+PS C:\Users\cesvh\dev\github\php>
+PS C:\Users\cesvh\dev\github\php>
+PS C:\Users\cesvh\dev\github\php>
+PS C:\Users\cesvh\dev\github\php> ls
+
+
+    Directorio: C:\Users\cesvh\dev\github\php
+
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d-----     30/07/2024  11:08 p. m.                core
+d-----     14/07/2024  02:05 p. m.                laravel
+-a----     14/07/2024  02:02 p. m.            511 .gitignore
+-a----     12/03/2024  12:23 a. m.           1083 LICENSE
+-a----     15/05/2024  12:11 a. m.           3210 README.md
+
+
+PS C:\Users\cesvh\dev\github\php>
+PS C:\Users\cesvh\dev\github\php>
+PS C:\Users\cesvh\dev\github\php>
+PS C:\Users\cesvh\dev\github\php>
+PS C:\Users\cesvh\dev\github\php>
+PS C:\Users\cesvh\dev\github\php> php artisan make:request ItemStoreRequest
+Could not open input file: artisan
+PS C:\Users\cesvh\dev\github\php>
+PS C:\Users\cesvh\dev\github\php>
+PS C:\Users\cesvh\dev\github\php>
+PS C:\Users\cesvh\dev\github\php>
+PS C:\Users\cesvh\dev\github\php>
+PS C:\Users\cesvh\dev\github\php> cd .\laravel\projects\web\tutos\techsolutionstuff-crud\
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud> php artisan make:request ItemStoreRequest
+
+   INFO  Request [C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud\app\Http\Requests\ItemStoreRequest.php] created successfully.
+
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud> php artisan make:request ItemUpdateRequest
+
+   INFO  Request [C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud\app\Http\Requests\ItemUpdateRequest.php] created successfully.
+
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
