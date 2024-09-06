9912c351806b3c8b17b2593eb10d16debd05cc73
cesvh <cesar.vega.hdez@gmail.com>
2024-09-05 22:23:19 -0600

    resource model controller

---
+++ laravel/projects/web/tutos/___logs/techsolutionstuff-crud/004-resource.sh
@@ -0,0 +1,20 @@
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud> php artisan make:controller ItemController --resource --model=Item
+
+  A App\Models\Item model does not exist. Do you want to generate it? (yes/no) [yes]
+❯ yes
+
+   INFO  Model [C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud\app\Models\Item.php] created successfully.
+
+   INFO  Controller [C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud\app\Http\Controllers\ItemController.php] created successfully.
+
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
+PS C:\Users\cesvh\dev\github\php\laravel\projects\web\tutos\techsolutionstuff-crud>
---
+++ laravel/projects/web/tutos/techsolutionstuff-crud/app/Http/Controllers/ItemController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Item;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\View\View;
+use App\Http\Requests\ItemStoreRequest;
+use App\Http\Requests\ItemUpdateRequest;
+
+class ItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $items = Item::latest()->paginate(5);
+          
+        return view('items.index', compact('items'))
+                    ->simplePaginate(10);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('items.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(ItemStoreRequest $request): RedirectResponse
+    {
+        Item::create($request->validated());
+           
+        return redirect()->route('items.index')
+                         ->with('success', 'Item created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Item $item)
+    {
+        return view('items.show', compact('item'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Item $item)
+    {
+        return view('items.edit', compact('item'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(ItemUpdateRequest $request, Item $item): RedirectResponse
+    {
+        $item->update($request->validated());
+          
+        return redirect()->route('items.index')
+                        ->with('success','Item updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Item $item): RedirectResponse
+    {
+        $item->delete();
+           
+        return redirect()->route('items.index')
+                        ->with('success','Item deleted successfully');
+    }
+}
