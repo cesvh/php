f5cbcfd229c9a541d11baceee05bc0c760a3b444
cesvh <96280230+cesvh@users.noreply.github.com>
2024-10-25 22:38:05 -0600

    Modificadores de acceso en php

---
+++ 002-oop/001-objects-class/004-access-modifiers.php
@@ -0,0 +1,24 @@
+<?php
+
+class Customer {
+	private $name;
+
+	public function setName($name) {
+		$name = trim($name);
+		if ($name == '') {
+			return false;
+		}
+		$this->name = $name;
+		return true;
+	}
+
+	public function getName() {
+		return $this->name;
+	}
+}
+
+$customer = new Customer();
+$customer->setName("Yun Tianming");
+$name = $customer->getName();
+
+echo $name;
