07a9216069a71828493f187de0b1629e3da5351e
cesvh <96280230+cesvh@users.noreply.github.com>
2024-10-28 15:52:24 -0600

    Constructor en php

---
+++ 002-oop/002-contructor-destructor/001-constructor.php
@@ -0,0 +1,20 @@
+<?php
+
+class BankAccount {
+	private $accountNumber;
+	private $balance;
+
+	public function __construct($accountNumber, $balance) {
+		$this->accountNumber = $accountNumber;
+		$this->balance = $balance;
+	}
+
+	public function showData(){
+		return $this->accountNumber . " - " . $this->balance;
+	}
+}
+
+$account = new BankAccount(1, 100);
+$msg = $account->showData();
+
+echo $msg;
