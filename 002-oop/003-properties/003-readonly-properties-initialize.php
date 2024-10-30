29d28c6fdd9979065b67e534deb7f5424a1fa7f9
cesvh <96280230+cesvh@users.noreply.github.com>
2024-10-30 15:51:19 -0600

    Inicializar propiedades de solo lectura en php

---
+++ 002-oop/003-properties/003-readonly-properties-initialize.php
@@ -0,0 +1,15 @@
+<?php
+
+
+class User {
+    public readonly string $username;
+    public string $password;
+
+    public function setUsername(string $username): void {
+        $this->username = $username;
+    }
+    
+}
+
+$user = new User();
+$user->setUsername('joe');
