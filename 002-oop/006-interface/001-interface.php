8c23ccf1f94c876ae0f309fe916a8a374c9b23e0
cesvh <96280230+cesvh@users.noreply.github.com>
2024-11-08 15:34:52 -0600

    interfaces en php

---
+++ 002-oop/006-interface/001-interface.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * 
+ * Una interfaz consta de métodos que no contienen ninguna implementación. 
+ * En otras palabras, todos los métodos de la interfaz son métodos abstractos. 
+ * Una interfaz también puede incluir constantes.
+ * 
+ * Todos los métodos de la interfaz deben ser públicos.
+ * Sin embargo, para las interfaces, decimos que una clase implementa una interfaz.
+ * 
+ * Una clase puede heredar de una sola clase, pero puede implementar múltiples interfaces.
+ * 
+ * Cuando una clase implementa una interfaz, se denomina clase concreta. La clase concreta debe implementar todos los métodos de la interfaz.
+ * 
+ * Al igual que una clase, una interfaz puede extender otra interfaz mediante la palabra clave extends.
+ * 
+ */
+
+/*
+interface MyInterface
+{
+	const CONSTANT_NAME = 1;
+
+	public function methodName();
+}
+
+class MyClass implements MyInterface
+{
+	public function methodName()
+	{
+		// ...
+	}
+}
+*/
+
+/**
+ * ¿Por qué deberías utilizar interfaces PHP?
+ * 
+ */
