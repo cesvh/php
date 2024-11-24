f70484edb8b6c9613f2fc3d9afbe528faa5b68ff
cesvh <cesar.vega.hdez@gmail.com>
2024-11-23 23:58:44 -0600

    feat: 001 Primeros pasos
    Formas de crear un proyecto en laravel

---
+++ laravel/laravel-11/docs-11.x/001-primeros-pasos/001-crear-proyecto-laravel.sh
@@ -0,0 +1,12 @@
+
+# Crear proyecto en laravel con composer
+composer create-project laravel/laravel example-app
+
+# Crear proyecto con laravel
+composer global require laravel/installer
+ 
+laravel new example-app
+
+cd example-app
+ 
+php artisan serve
