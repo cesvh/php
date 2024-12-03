bd0542a5f0130052baceb6f944b802a27ec978e8
cesvh <96280230+cesvh@users.noreply.github.com>
2024-12-03 15:38:46 -0600

    feat: array en php

---
+++ 001-core/003-data-types/002-compound/001-array.php
@@ -0,0 +1,21 @@
+<?php
+
+// Array
+$mi_arreglo = [299, 792, 458];
+
+var_dump($mi_arreglo);
+var_dump($mi_arreglo[0]);
+var_dump($mi_arreglo[1]);
+
+// Array asociativo
+$mi_arreglo2 = [
+  "miles" => 299,
+  "cientos" =>792,
+  "decimales" => 458
+];
+
+var_dump($mi_arreglo2);
+var_dump($mi_arreglo2["miles"]);
+var_dump($mi_arreglo2["cientos"]);
+
+?>
