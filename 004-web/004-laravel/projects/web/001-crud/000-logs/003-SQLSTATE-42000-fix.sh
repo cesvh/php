22446842117838abd2ffbb3cc54e007fdc0a99fa
cesvh <cesar.vega.hdez@gmail.com>
2024-12-02 23:15:16 -0600

    fix:  SQLSTATE[42000]: Syntax error or access violation: 1071
     SQLSTATE[42000]: Syntax error or access violation: 1071 Specified key was too long; max key length is 1000 bytes (Connection: mysql, SQL: alter table  add unique ())

---
+++ 004-web/004-laravel/projects/web/001-crud/000-logs/003-SQLSTATE-42000-fix.sh
@@ -0,0 +1,17 @@
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\001-qadrlabs.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\001-qadrlabs.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\001-qadrlabs.com-crud> php artisan migrate
+
+   INFO  Preparing database.
+
+  Creating migration table ............................................................................................................ 12.32ms DONE
+
+   INFO  Running migrations.
+
+  0001_01_01_000000_create_users_table ............................................................................................... 138.64ms DONE
+  0001_01_01_000001_create_cache_table ................................................................................................ 18.59ms DONE
+  0001_01_01_000002_create_jobs_table ................................................................................................ 145.34ms DONE
+
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\001-qadrlabs.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\001-qadrlabs.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\001-qadrlabs.com-crud>
