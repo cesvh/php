9785ad3ebb4e2e787b6ba24bd11b13aa2efa5cad
cesvh <96280230+cesvh@users.noreply.github.com>
2024-12-04 16:59:49 -0600

    fix: bool en php

--- 001-core/003-data-types/001-scalar/003-boolean.php
+++ 001-core/003-data-types/001-scalar/003-boolean.php
@@ -3,7 +3,19 @@
 $valor1 = true;
 $valor2 = false;
 
+$integer_zero = 0;
+$float_zero = 0.0;
+$empty_string = '';
+$string_zero = "0";
+$null_value = null;
+
 var_dump($valor1);
 var_dump($valor2);
+
+var_dump(is_bool($integer_zero));
+var_dump(is_bool($float_zero));
+var_dump(is_bool($empty_string));
+var_dump(is_bool($string_zero));
+var_dump(is_bool($null_value));
 
 ?>
