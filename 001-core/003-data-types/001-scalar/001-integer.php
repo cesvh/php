c35c76d0a4eb7797b76d3b00ae79cea99e693c7b
cesvh <96280230+cesvh@users.noreply.github.com>
2024-12-05 16:07:54 -0600

    fix: integer in php

--- 001-core/003-data-types/001-scalar/001-integer.php
+++ 001-core/003-data-types/001-scalar/001-integer.php
@@ -4,4 +4,11 @@ $year = 1990;
 var_dump(gettype($year));
 var_dump($year);
 
+var_dump(PHP_INT_SIZE);
+var_dump(PHP_INT_MIN);
+var_dump(PHP_INT_MAX);
+
+$great = 999_999_999_999_999_999;
+var_dump($great);
+
 ?>
