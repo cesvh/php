610b1f305d57291da7cfa415c1d4b35100fd2f7b
cesvh <96280230+cesvh@users.noreply.github.com>
2024-12-10 14:38:35 -0600

    type casting en php

---
+++ 001-core/003-data-types/001-scalar/005-type-casting.php
@@ -0,0 +1,12 @@
+<?php
+
+$int_value = 299.792;
+
+var_dump($int_value);
+var_dump((int)$int_value);
+
+$str_value = "1991 años";
+var_dump((int)$str_value);
+
+$str_value2 = "años";
+var_dump((int)$str_value2);
