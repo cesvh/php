b7917c0225b1c5fde505f813e5a25a2cf75ddc96
cesvh <cesar.vega.hdez@gmail.com>
2024-12-12 00:31:05 -0600

    feat: controller y resource user
    php artisan make:controller UserController --model=User --resource

---
+++ 004-web/004-laravel/projects/web/001-crud/000-logs/004-php-artisan-make-controller.sh
@@ -0,0 +1,10 @@
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\001-qadrlabs.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\001-qadrlabs.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\001-qadrlabs.com-crud> php artisan make:controller UserController --model=User --resource
+
+   INFO  Controller [C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\001-qadrlabs.com-crud\app\Http\Controllers\UserController.php] created successfully.
+
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\001-qadrlabs.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\001-qadrlabs.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\001-qadrlabs.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\001-qadrlabs.com-crud>
