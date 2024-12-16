6f1ae68cb0f240a769df1e592c07bd253b89c1b3
cesvh <cesar.vega.hdez@gmail.com>
2024-12-15 22:42:02 -0600

    fix: migrate db error
    fix migrate db error

---
+++ 004-web/004-laravel/projects/web/001-crud/000-logs/002-itsolutionstuff.com-crud/003-fix-php-artisan-migrate.sh
@@ -0,0 +1,19 @@
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\002-itsolutionstuff.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\002-itsolutionstuff.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\002-itsolutionstuff.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\002-itsolutionstuff.com-crud> php artisan migrate
+
+   INFO  Preparing database.
+
+  Creating migration table ............................................................................................................ 22.42ms DONE
+
+   INFO  Running migrations.
+
+  0001_01_01_000000_create_users_table ............................................................................................... 206.23ms DONE
+  0001_01_01_000001_create_cache_table ................................................................................................ 42.75ms DONE
+  0001_01_01_000002_create_jobs_table ................................................................................................ 189.64ms DONE
+
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\002-itsolutionstuff.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\002-itsolutionstuff.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\002-itsolutionstuff.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\002-itsolutionstuff.com-crud>
