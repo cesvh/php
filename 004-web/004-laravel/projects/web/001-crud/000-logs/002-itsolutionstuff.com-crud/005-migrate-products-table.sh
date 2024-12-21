5b233e450d6056cb3cc5a09a2515f598d7012d34
cesvh <cesar.vega.hdez@gmail.com>
2024-12-21 01:24:28 -0600

    feat: migrate products table
    php artisan migrate

---
+++ 004-web/004-laravel/projects/web/001-crud/000-logs/002-itsolutionstuff.com-crud/005-migrate-products-table.sh
@@ -0,0 +1,13 @@
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\002-itsolutionstuff.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\002-itsolutionstuff.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\002-itsolutionstuff.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\002-itsolutionstuff.com-crud> php artisan migrate
+
+   INFO  Running migrations.
+
+  2024_12_20_040951_create_products_table ............................................................................................ 324.88ms DONE
+
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\002-itsolutionstuff.com-crud> 
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\002-itsolutionstuff.com-crud> 
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\002-itsolutionstuff.com-crud> 
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\002-itsolutionstuff.com-crud> 
