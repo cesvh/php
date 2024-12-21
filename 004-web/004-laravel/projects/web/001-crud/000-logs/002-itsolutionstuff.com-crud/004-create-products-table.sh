8ec26c3065181bd951beb11643525c4e5a890d4f
cesvh <cesar.vega.hdez@gmail.com>
2024-12-20 19:49:09 -0600

    feat: create products table
    php artisan make:migration create_products_table --create=products

---
+++ 004-web/004-laravel/projects/web/001-crud/000-logs/002-itsolutionstuff.com-crud/004-create-products-table.sh
@@ -0,0 +1,8 @@
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\002-itsolutionstuff.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\002-itsolutionstuff.com-crud> php artisan make:migration create_products_table --create=products
+
+   INFO  Migration [C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\002-itsolutionstuff.com-crud\database\migrations/2024_12_20_040951_create_products_table.php] created successfully.
+
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\002-itsolutionstuff.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\002-itsolutionstuff.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\002-itsolutionstuff.com-crud>
