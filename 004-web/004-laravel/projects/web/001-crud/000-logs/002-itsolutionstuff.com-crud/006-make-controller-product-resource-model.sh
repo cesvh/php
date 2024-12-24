5ae9f5406d394d827c1f1a06161198decd3e801d
cesvh <cesar.vega.hdez@gmail.com>
2024-12-23 22:33:34 -0600

    feat: models and logs

---
+++ 004-web/004-laravel/projects/web/001-crud/000-logs/002-itsolutionstuff.com-crud/006-make-controller-product-resource-model.sh
@@ -0,0 +1,20 @@
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\002-itsolutionstuff.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\002-itsolutionstuff.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\002-itsolutionstuff.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\002-itsolutionstuff.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\002-itsolutionstuff.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\002-itsolutionstuff.com-crud> php artisan make:controller ProductController --resource --model=Product
+
+  A App\Models\Product model does not exist. Do you want to generate it? (yes/no) [yes]
+❯ yes
+
+   INFO  Model [C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\002-itsolutionstuff.com-crud\app\Models\Product.php] created successfully.
+
+   INFO  Controller [C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\002-itsolutionstuff.com-crud\app\Http\Controllers\ProductController.php] created successfully.
+
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\002-itsolutionstuff.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\002-itsolutionstuff.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\002-itsolutionstuff.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\002-itsolutionstuff.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\002-itsolutionstuff.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\002-itsolutionstuff.com-crud>
