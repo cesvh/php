4122bdf1b086fd9ffed05362dd86a04a9c136407
cesvh <cesar.vega.hdez@gmail.com>
2025-01-01 20:22:26 -0600

    feat: php artisan make:model Product -mcr --requests
    model, resource, controller and requests

---
+++ 004-web/004-laravel/projects/web/001-crud/000-logs/003-allphptricks.com-crud/002-model-resource-controller-requests.sh
@@ -0,0 +1,49 @@
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\003-allphptricks.com-crud> ls
+
+    Directory: C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\003-allphptricks.com-crud
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d----       13/12/2024 07:57 a. m.                app
+d----       13/12/2024 07:57 a. m.                bootstrap
+d----       13/12/2024 07:57 a. m.                config
+d----       01/01/2025 07:40 p. m.                database
+d----       13/12/2024 07:57 a. m.                public
+d----       13/12/2024 07:57 a. m.                resources
+d----       13/12/2024 07:57 a. m.                routes
+d----       13/12/2024 07:57 a. m.                storage
+d----       13/12/2024 07:57 a. m.                tests
+d----       01/01/2025 07:38 p. m.                vendor
+-----       13/12/2024 07:57 a. m.            258 .editorconfig
+-a---       01/01/2025 08:09 p. m.           1151 .env
+-----       13/12/2024 07:57 a. m.           1099 .env.example
+-----       13/12/2024 07:57 a. m.            186 .gitattributes
+-----       13/12/2024 07:57 a. m.            285 .gitignore
+-----       13/12/2024 07:57 a. m.            350 artisan
+-----       13/12/2024 07:57 a. m.           2306 composer.json
+-a---       01/01/2025 07:32 p. m.         297218 composer.lock
+-----       13/12/2024 07:57 a. m.            378 package.json
+-----       13/12/2024 07:57 a. m.           1191 phpunit.xml
+-----       13/12/2024 07:57 a. m.             93 postcss.config.js
+-----       13/12/2024 07:57 a. m.           4109 README.md
+-----       13/12/2024 07:57 a. m.            551 tailwind.config.js
+-----       13/12/2024 07:57 a. m.            263 vite.config.js
+
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\003-allphptricks.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\003-allphptricks.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\003-allphptricks.com-crud> php artisan make:model Product -mcr --requests
+
+   INFO  Model [C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\003-allphptricks.com-crud\app\Models\Product.php] created successfully.
+
+   INFO  Migration [C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\003-allphptricks.com-crud\database\migrations/2025_01_02_021745_create_products_table.php] created successfully.
+
+   INFO  Request [C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\003-allphptricks.com-crud\app\Http\Requests\StoreProductRequest.php] created successfully.
+
+   INFO  Request [C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\003-allphptricks.com-crud\app\Http\Requests\UpdateProductRequest.php] created successfully.
+
+   INFO  Controller [C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\003-allphptricks.com-crud\app\Http\Controllers\ProductController.php] created successfully.
+
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\003-allphptricks.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\003-allphptricks.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\003-allphptricks.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\003-allphptricks.com-crud>
