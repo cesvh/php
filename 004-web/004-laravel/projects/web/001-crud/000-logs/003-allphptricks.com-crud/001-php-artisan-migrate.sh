9ac4bec8180fca331ad9039fdd0ed2e3372794e5
cesvh <cesar.vega.hdez@gmail.com>
2025-01-01 20:12:20 -0600

    feat: db config
    php artisan migrate

---
+++ 004-web/004-laravel/projects/web/001-crud/000-logs/003-allphptricks.com-crud/001-php-artisan-migrate.sh
@@ -0,0 +1,70 @@
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud> ls
+
+    Directory: C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d----       01/01/2025 07:40 p. m.                000-logs
+d----       30/11/2024 11:22 a. m.                001-qadrlabs.com-crud
+d----       15/12/2024 07:22 p. m.                002-itsolutionstuff.com-crud
+d----       01/01/2025 07:32 p. m.                003-allphptricks.com-crud
+
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud> cd .\003-allphptricks.com-crud\
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\003-allphptricks.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\003-allphptricks.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\003-allphptricks.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\003-allphptricks.com-crud> ls
+
+    Directory: C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\003-allphptricks.com-crud
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d----       13/12/2024 07:57 a. m.                app
+d----       13/12/2024 07:57 a. m.                bootstrap
+d----       13/12/2024 07:57 a. m.                config
+d----       01/01/2025 07:40 p. m.                database
+d----       13/12/2024 07:57 a. m.                public
+d----       13/12/2024 07:57 a. m.                resources
+d----       13/12/2024 07:57 a. m.                routes
+d----       13/12/2024 07:57 a. m.                storage
+d----       13/12/2024 07:57 a. m.                tests
+d----       01/01/2025 07:38 p. m.                vendor
+-----       13/12/2024 07:57 a. m.            258 .editorconfig
+-a---       01/01/2025 08:09 p. m.           1151 .env
+-----       13/12/2024 07:57 a. m.           1099 .env.example
+-----       13/12/2024 07:57 a. m.            186 .gitattributes
+-----       13/12/2024 07:57 a. m.            285 .gitignore
+-----       13/12/2024 07:57 a. m.            350 artisan
+-----       13/12/2024 07:57 a. m.           2306 composer.json
+-a---       01/01/2025 07:32 p. m.         297218 composer.lock
+-----       13/12/2024 07:57 a. m.            378 package.json
+-----       13/12/2024 07:57 a. m.           1191 phpunit.xml
+-----       13/12/2024 07:57 a. m.             93 postcss.config.js
+-----       13/12/2024 07:57 a. m.           4109 README.md
+-----       13/12/2024 07:57 a. m.            551 tailwind.config.js
+-----       13/12/2024 07:57 a. m.            263 vite.config.js
+
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\003-allphptricks.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\003-allphptricks.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\003-allphptricks.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\003-allphptricks.com-crud> php artisan migrate
+
+   INFO  Preparing database.
+
+  Creating migration table ........................................................................................................... 547.20ms DONE
+
+   INFO  Running migrations.
+
+  0001_01_01_000000_create_users_table ............................................................................................... 364.42ms DONE
+  0001_01_01_000001_create_cache_table ................................................................................................ 26.28ms DONE
+  0001_01_01_000002_create_jobs_table ................................................................................................ 267.72ms DONE
+
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\003-allphptricks.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\003-allphptricks.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\003-allphptricks.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\003-allphptricks.com-crud>
