eaaae333e086e9cadba2a99bb5182604d141de18
cesvh <cesar.vega.hdez@gmail.com>
2025-01-01 21:36:49 -0600

    feat: detalles de las vistas

--- 004-web/004-laravel/projects/web/001-crud/003-allphptricks.com-crud/resources/views/products/show.blade.php
+++ 004-web/004-laravel/projects/web/001-crud/003-allphptricks.com-crud/resources/views/products/show.blade.php
@@ -1,3 +1,49 @@
-<div>
-    <!-- I have not failed. I've just found 10,000 ways that won't work. - Thomas Edison -->
+@extends('layouts.app')
+@section('content')
+<div class="row justify-content-center mt-3">
+    <div class="col-md-8">
+        <div class="card">
+            <div class="card-header">
+                <div class="float-start">
+                    Información del producto
+                </div>
+                <div class="float-end">
+                    <a href="{{ route('products.index') }}" class="btn btn-primary btn-sm">&larr; Regresar</a>
+                </div>
+            </div>
+            <div class="card-body">
+                    <div class="row">
+                        <label for="code" class="col-md-4 col-form-label text-md-end text-start"><strong>Código:</strong></label>
+                        <div class="col-md-6" style="line-height: 35px;">
+                            {{ $product->code }}
+                        </div>
+                    </div>
+                    <div class="row">
+                        <label for="name" class="col-md-4 col-form-label text-md-end text-start"><strong>Nombre:</strong></label>
+                        <div class="col-md-6" style="line-height: 35px;">
+                            {{ $product->name }}
+                        </div>
+                    </div>
+                    <div class="row">
+                        <label for="quantity" class="col-md-4 col-form-label text-md-end text-start"><strong>Cantidad:</strong></label>
+                        <div class="col-md-6" style="line-height: 35px;">
+                            {{ $product->quantity }}
+                        </div>
+                    </div>
+                    <div class="row">
+                        <label for="price" class="col-md-4 col-form-label text-md-end text-start"><strong>Precio:</strong></label>
+                        <div class="col-md-6" style="line-height: 35px;">
+                            {{ $product->price }}
+                        </div>
+                    </div>
+                    <div class="row">
+                        <label for="description" class="col-md-4 col-form-label text-md-end text-start"><strong>Descripción:</strong></label>
+                        <div class="col-md-6" style="line-height: 35px;">
+                            {{ $product->description }}
+                        </div>
+                    </div>
+            </div>
+        </div>
+    </div>    
 </div>
+@endsection
