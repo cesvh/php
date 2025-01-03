83d46645644a33a413a180ee0b4d16b1977a9e44
cesvh <96280230+cesvh@users.noreply.github.com>
2025-01-03 11:10:23 -0600

    Multiplicación en PHP

--- 001-core/006-functions/001-funciones.php
+++ 001-core/006-functions/001-funciones.php
@@ -2,12 +2,19 @@
 
 $res = 0;
 
-function potencia($num1, $num2, &$res = 0) {
-    $res = $num1 + $num2;
-    echo "Resultado es la potencia: " . $res;
+function multiplicacion($num1, $num2, &$res = 0) {
+    $res = $num1 ** $num2;
+    echo "Resultado de la multiplicacion: " . $res;
 }
 
-potencia(5, 5, $res);
+multiplicacion(5, 5, $res);
 
 echo "\r\n";
 echo $res;
+
+/*
+Output:
+
+Resultado de la multiplicacion: 3125
+3125
+*/
