aa440798a8a6bc4c026aa7c7d09bd316dccc7e53
cesvh <96280230+cesvh@users.noreply.github.com>
2025-01-03 15:14:41 -0600

    feat: Parámetros default en PHP

--- 001-core/006-functions/003-parametros-default.php
+++ 001-core/006-functions/003-parametros-default.php
@@ -1,5 +1,13 @@
 <?php
 
-# PENDIENTE
+function crearFolio($usuario, $correo, $caracter = "") {
+    echo "folio: \"" . $usuario . $caracter . $correo . "\"";
+}
+
+crearFolio("ces", "cvh@gmail.com", "-");
 
 /*
+Output:
+
+Resultado de la multiplicacion: 25
+*/
