980a21624b30365e0e55d854f9e42a2c30a5d158
cesvh <96280230+cesvh@users.noreply.github.com>
2025-01-03 14:37:14 -0600

    fix: Corrección en funciones de PHP

--- 001-core/006-functions/001-funciones.php
+++ 001-core/006-functions/001-funciones.php
@@ -2,19 +2,19 @@
 
 $res = 0;
 
-function potencia($num1, $num2, &$res = 0) {
-    $res = $num1 ** $num2;
-    echo "Resultado de la potencia: " . $res;
+function multiplicacion($num1, $num2, &$res = 0) {
+    $res = $num1 * $num2;
+    echo "Resultado de la multiplicacion: " . $res;
 }
 
-potencia(5, 5, $res);
+multiplicacion(5, 5, $res);
 
 echo "\r\n";
-echo $res;
+echo "Resultado en \$res: " . $res;
 
 /*
 Output:
 
-Resultado de la potencia: 3125
-3125
+Resultado de la multiplicacion: 25
+Resultado en $res: 25
 */
