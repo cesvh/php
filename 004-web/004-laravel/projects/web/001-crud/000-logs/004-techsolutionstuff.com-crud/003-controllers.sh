705fcdb220142db4235fb89f9a62ccabd71e52ab
cesvh <cesar.vega.hdez@gmail.com>
2025-01-04 23:00:17 -0600

    feat: vistas

---
+++ 004-web/004-laravel/projects/web/001-crud/000-logs/004-techsolutionstuff.com-crud/003-controllers.sh
@@ -0,0 +1,52 @@
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\004-techsolutionstuff.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\004-techsolutionstuff.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\004-techsolutionstuff.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\004-techsolutionstuff.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\004-techsolutionstuff.com-crud> ls
+
+    Directory: C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\004-techsolutionstuff.com-crud
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d----       02/01/2025 10:21 p. m.                app
+d----       02/01/2025 10:21 p. m.                bootstrap
+d----       02/01/2025 10:22 p. m.                config
+d----       02/01/2025 10:30 p. m.                database
+d----       02/01/2025 10:22 p. m.                public
+d----       02/01/2025 10:22 p. m.                resources
+d----       02/01/2025 10:22 p. m.                routes
+d----       02/01/2025 10:22 p. m.                storage
+d----       02/01/2025 10:22 p. m.                tests
+d----       02/01/2025 10:30 p. m.                vendor
+-a---       02/01/2025 10:21 p. m.            258 .editorconfig
+-a---       02/01/2025 10:52 p. m.           1156 .env
+-a---       02/01/2025 10:21 p. m.           1099 .env.example
+-a---       02/01/2025 10:21 p. m.            186 .gitattributes
+-a---       02/01/2025 10:21 p. m.            285 .gitignore
+-a---       02/01/2025 10:21 p. m.            350 artisan
+-a---       02/01/2025 10:21 p. m.           2306 composer.json
+-a---       02/01/2025 10:22 p. m.         297174 composer.lock
+-a---       02/01/2025 10:22 p. m.            378 package.json
+-a---       02/01/2025 10:22 p. m.           1191 phpunit.xml
+-a---       02/01/2025 10:22 p. m.             93 postcss.config.js
+-a---       02/01/2025 10:22 p. m.           4109 README.md
+-a---       02/01/2025 10:22 p. m.            551 tailwind.config.js
+-a---       02/01/2025 10:22 p. m.            263 vite.config.js
+
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\004-techsolutionstuff.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\004-techsolutionstuff.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\004-techsolutionstuff.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\004-techsolutionstuff.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\004-techsolutionstuff.com-crud> php artisan make:controller ItemController --resource --model=Item
+
+  A App\Models\Item model does not exist. Do you want to generate it? (yes/no) [yes]
+❯ yes
+
+   INFO  Model [C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\004-techsolutionstuff.com-crud\app\Models\Item.php] created successfully.
+
+   INFO  Controller [C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\004-techsolutionstuff.com-crud\app\Http\Controllers\ItemController.php] created successfully.
+
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\004-techsolutionstuff.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\004-techsolutionstuff.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\004-techsolutionstuff.com-crud>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\001-crud\004-techsolutionstuff.com-crud>
