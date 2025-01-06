bd9a13ed73aae9f00effabc270010fa4f1dff3f6
cesvh <96280230+cesvh@users.noreply.github.com>
2025-01-06 17:00:55 -0600

    feat: sugerencia de tipos en php

--- 001-core/006-functions/006-sugerencias-de-tipo.php
+++ 001-core/006-functions/006-sugerencias-de-tipo.php
@@ -1,5 +1,17 @@
 <?php
 
-# PENDIENTE
+function multiplicacion(float $num1, float $num2): float {
+    $res = $num1 * $num2;
+    return (float) $res;
+}
+
+$res = multiplicacion(5.5, 5.5);
+
+echo "\r\n";
+echo "Resultado en \$res: " . $res;
 
 /*
+Output:
+
+Resultado en $res: 30.25
+*/
