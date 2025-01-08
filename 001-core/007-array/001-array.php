808fd54faf8bf8e540dafcaedbe5d01a7d81aaa3
cesvh <96280230+cesvh@users.noreply.github.com>
2025-01-08 16:53:44 -0600

    fix: array en php

--- 001-core/007-array/001-array.php
+++ 001-core/007-array/001-array.php
@@ -3,4 +3,22 @@
 $array = ["foo", "bar", "hello", "world"];
 var_dump($array);
 
+/*
+
+Output:
+
+array(4) {
+  [0]=>
+  string(3) "foo"
+  [1]=>
+  string(3) "bar"
+  [2]=>
+  string(5) "hello"
+  [3]=>
+  string(5) "world"
+}
+
+*/
+
+
 ?>
