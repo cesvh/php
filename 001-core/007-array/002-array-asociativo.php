3025fd1dfaa4514602a79e8abfe2c29214b591ea
cesvh <96280230+cesvh@users.noreply.github.com>
2025-01-09 15:07:18 -0600

    fix: array asociativo en php

--- 001-core/007-array/002-array-asociativo.php
+++ 001-core/007-array/002-array-asociativo.php
@@ -2,19 +2,11 @@
 
 $array = array(
     "foo" => "bar",
-    42    => 24,
-    "multi" => array(
-         "dimensional" => array(
-             "array" => "foo"
-         )
-    )
+    42    => 24
 );
 
 var_dump($array["foo"]);
 var_dump($array[42]);
-var_dump($array["multi"]);
-var_dump($array["multi"]["dimensional"]);
-var_dump($array["multi"]["dimensional"]["array"]);
 
 /*
 
@@ -22,18 +14,6 @@ Output:
 
 string(3) "bar"
 int(24)
-array(1) {
-  ["dimensional"]=>
-  array(1) {
-    ["array"]=>
-    string(3) "foo"
-  }
-}
-array(1) {
-  ["array"]=>
-  string(3) "foo"
-}
-string(3) "foo"
 
 */
 
