8b0fbf48b07134d8d317d050e4760f75d10caaf4
cesvh <cesar.vega.hdez@gmail.com>
2025-01-09 20:41:47 -0600

    feat new api project

---
+++ 004-web/004-laravel/projects/web/002-api/000-logs/001-medium-shaillydixit999-api/002-php-artisan-install-api.sh
@@ -0,0 +1,149 @@
+PowerShell 7.4.6
+PS C:\Windows\System32>
+PS C:\Windows\System32>
+PS C:\Windows\System32>
+PS C:\Windows\System32> cd C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web> ls
+
+    Directory: C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d----       02/01/2025 10:22 p. m.                001-crud
+d----       04/01/2025 11:49 p. m.                002-api
+
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web> cd .\002-api\
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api> ls
+
+    Directory: C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d----       04/01/2025 11:55 p. m.                000-logs
+d----       04/01/2025 11:58 p. m.                001-api-basico
+d----       04/01/2025 11:49 p. m.                002-api-jwt
+
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api> cd .\001-api-basico\
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico> ls
+
+    Directory: C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d----       04/01/2025 11:58 p. m.                001-medium-shaillydixit999
+
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico> cd .\001-medium-shaillydixit999\
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico\001-medium-shaillydixit999>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico\001-medium-shaillydixit999>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico\001-medium-shaillydixit999>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico\001-medium-shaillydixit999> ls
+
+    Directory: C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico\001-medium-shaillydixit999
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d----       13/12/2024 07:57 a. m.                app
+d----       13/12/2024 07:57 a. m.                bootstrap
+d----       13/12/2024 07:57 a. m.                config
+d----       05/01/2025 12:05 a. m.                database
+d----       13/12/2024 07:57 a. m.                public
+d----       13/12/2024 07:57 a. m.                resources
+d----       13/12/2024 07:57 a. m.                routes
+d----       13/12/2024 07:57 a. m.                storage
+d----       13/12/2024 07:57 a. m.                tests
+d----       05/01/2025 12:04 a. m.                vendor
+-----       13/12/2024 07:57 a. m.            258 .editorconfig
+-a---       05/01/2025 12:04 a. m.           1150 .env
+-----       13/12/2024 07:57 a. m.           1099 .env.example
+-----       13/12/2024 07:57 a. m.            186 .gitattributes
+-----       13/12/2024 07:57 a. m.            285 .gitignore
+-----       13/12/2024 07:57 a. m.            350 artisan
+-----       13/12/2024 07:57 a. m.           2306 composer.json
+-a---       04/01/2025 11:58 p. m.         297174 composer.lock
+-----       13/12/2024 07:57 a. m.            378 package.json
+-----       13/12/2024 07:57 a. m.           1191 phpunit.xml
+-----       13/12/2024 07:57 a. m.             93 postcss.config.js
+-----       13/12/2024 07:57 a. m.           4109 README.md
+-----       13/12/2024 07:57 a. m.            551 tailwind.config.js
+-----       13/12/2024 07:57 a. m.            263 vite.config.js
+
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico\001-medium-shaillydixit999>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico\001-medium-shaillydixit999>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico\001-medium-shaillydixit999>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico\001-medium-shaillydixit999>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico\001-medium-shaillydixit999>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico\001-medium-shaillydixit999>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico\001-medium-shaillydixit999> php artisan install:api
+./composer.json has been updated
+Running composer update laravel/sanctum
+Loading composer repositories with package information
+Updating dependencies
+Lock file operations: 1 install, 0 updates, 0 removals
+  - Locking laravel/sanctum (v4.0.7)
+Writing lock file
+Installing dependencies from lock file (including require-dev)
+Package operations: 1 install, 0 updates, 0 removals
+  - Downloading laravel/sanctum (v4.0.7)
+  - Installing laravel/sanctum (v4.0.7): Extracting archive
+Generating optimized autoload files
+> Illuminate\Foundation\ComposerScripts::postAutoloadDump
+> @php artisan package:discover --ansi
+
+   INFO  Discovering packages.
+
+  laravel/pail ................................................................................................................................ DONE
+  laravel/sail ................................................................................................................................ DONE
+  laravel/sanctum ............................................................................................................................. DONE
+  laravel/tinker .............................................................................................................................. DONE
+  nesbot/carbon ............................................................................................................................... DONE
+  nunomaduro/collision ........................................................................................................................ DONE
+  nunomaduro/termwind ......................................................................................................................... DONE
+
+81 packages you are using are looking for funding.
+Use the `composer fund` command to find out more!
+> @php artisan vendor:publish --tag=laravel-assets --ansi --force
+
+   INFO  No publishable resources for tag [laravel-assets].
+
+No security vulnerability advisories found.
+   INFO  Published API routes file.
+
+ One new database migration has been published. Would you like to run all pending database migrations? (yes/no) [yes]:
+ >
+
+   INFO  Running migrations.
+
+  2025_01_07_032723_create_personal_access_tokens_table ............................................................................... 69.48ms DONE
+
+
+   INFO  API scaffolding installed. Please add the [Laravel\Sanctum\HasApiTokens] trait to your User model.
+
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico\001-medium-shaillydixit999>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico\001-medium-shaillydixit999>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico\001-medium-shaillydixit999>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico\001-medium-shaillydixit999>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico\001-medium-shaillydixit999>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico\001-medium-shaillydixit999>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico\001-medium-shaillydixit999>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico\001-medium-shaillydixit999>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico\001-medium-shaillydixit999>
