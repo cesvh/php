9baa2cb566967bbf14113a997cd124db9b0a2bf5
cesvh <96280230+cesvh@users.noreply.github.com>
2025-01-13 14:36:08 -0600

    feat: array_push en php

---
+++ 001-core/007-array/007-push.php
@@ -0,0 +1,20 @@
+<?php
+
+$numbers = [1, 2, 3];
+array_push($numbers, 4, 5);
+print_r($numbers);
+
+/*
+Output:
+
+Array
+(
+    [0] => 1
+    [1] => 2
+    [2] => 3
+    [3] => 4
+    [4] => 5
+)
+*/
+
+?>
