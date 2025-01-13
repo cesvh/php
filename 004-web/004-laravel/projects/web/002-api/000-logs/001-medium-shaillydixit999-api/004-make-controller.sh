2c17d189b91c478112bd22ac8e73307b3919a20e
cesvh <cesar.vega.hdez@gmail.com>
2025-01-12 22:52:25 -0600

    feat: api routes

---
+++ 004-web/004-laravel/projects/web/002-api/000-logs/001-medium-shaillydixit999-api/004-make-controller.sh
@@ -0,0 +1,17 @@
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico\001-medium-shaillydixit999>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico\001-medium-shaillydixit999>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico\001-medium-shaillydixit999>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico\001-medium-shaillydixit999>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico\001-medium-shaillydixit999>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico\001-medium-shaillydixit999>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico\001-medium-shaillydixit999> php artisan make:controller CustomerController
+
+   INFO  Controller [C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico\001-medium-shaillydixit999\app\Http\Controllers\CustomerController.php] created successfully.
+
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico\001-medium-shaillydixit999>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico\001-medium-shaillydixit999>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico\001-medium-shaillydixit999>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico\001-medium-shaillydixit999>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico\001-medium-shaillydixit999>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico\001-medium-shaillydixit999>
+PS C:\Users\cesvh\dev\github\php\004-web\004-laravel\projects\web\002-api\001-api-basico\001-medium-shaillydixit999>
--- 004-web/004-laravel/projects/web/002-api/001-api-basico/001-medium-shaillydixit999/routes/api.php
+++ 004-web/004-laravel/projects/web/002-api/001-api-basico/001-medium-shaillydixit999/routes/api.php
@@ -3,6 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::get('/customers', [CustomerController::class, 'index']);
+Route::get('/customers/{id}', [CustomerController::class, 'show']);
+Route::post('/customers', [CustomerController::class, 'store']);
+Route::put('/customers/{id}', [CustomerController::class, 'update']);
+Route::delete('/customers/{id}', [CustomerController::class, 'destroy']);
