4f9f35fb36ae6986c90b6363aff4f47b51cd00e6
cesvh <96280230+cesvh@users.noreply.github.com>
2025-01-13 15:44:10 -0600

    fix: array_keys en php

--- 001-core/007-array/010-array_keys.php
+++ 001-core/007-array/010-array_keys.php
@@ -1,14 +1,23 @@
 <?php
 
-# Busca un valor en un array y devuelve un array asociativo de las claves donde coincide
-
 $numbers = [10, 20, 30, 30, 50, 60, 30];
 $keys = array_keys($numbers);
 print_r($keys);
 
-
 $keys = array_keys($numbers, 30);
 print_r($keys);
+
+$user = [
+	'username' => 'admin',
+	'email' => 'admin@phptutorial.net',
+	'is_active' => '1'
+];
+$properties = array_keys($user);
+print_r($properties);
+$properties = array_keys($user, 1);
+print_r($properties);
+$properties = array_keys($user, 1, true);
+print_r($properties);
 
 /*
 Output:
@@ -28,6 +37,19 @@ Array
     [0] => 2
     [1] => 3
     [2] => 6
+)
+Array
+(
+    [0] => username
+    [1] => email
+    [2] => is_active
+)
+Array
+(
+    [0] => is_active
+)
+Array
+(
 )
 */
 
