debb034b3e8a70a5da9db2f6ef25092d3489310f
cesvh <96280230+cesvh@users.noreply.github.com>
2025-01-13 16:21:44 -0600

    feat: array_keys_by en php

---
+++ 001-core/007-array/011-array_keys_by.php
@@ -0,0 +1,38 @@
+<?php
+
+function array_keys_by(array $array, callable $callback): array
+{
+	$keys = [];
+	foreach ($array as $key => $value) {
+		if ($callback($key)) {
+			$keys[] = $key;
+		}
+	}
+	return $keys;
+}
+
+$permissions = [
+	'edit_post' => 1,
+	'delete_post' => 2,
+	'publish_post' => 3,
+	'approve' => 4,
+];
+
+$keys = array_keys_by($permissions, function ($permission) {
+	return strpos($permission, 'post');
+});
+
+print_r($keys);
+
+/*
+Output:
+
+Array
+(
+    [0] => edit_post
+    [1] => delete_post
+    [2] => publish_post
+)
+*/
+
+?>
