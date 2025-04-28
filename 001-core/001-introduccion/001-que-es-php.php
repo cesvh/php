b89329732640a9b0f39534be3122b349e4b51fd5
cesvh <cesar.vega.hdez@gmail.com>
2025-04-28 15:33:16 -0600

    feat: php Crear servidor local sin wamp

--- 001-core/001-introduccion/001-que-es-php.php
+++ 001-core/001-introduccion/001-que-es-php.php
@@ -1,6 +1,7 @@
 <?php
 
-echo "The Last Question!";
-echo "\n";
+echo "¿?";
+// C:\Users\cesvh\dev\github\php\001-core\001-introduccion> php -S localhost:8080
+// http://localhost:8080/001-que-es-php.php
 
 ?>
